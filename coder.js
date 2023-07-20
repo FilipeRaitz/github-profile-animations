ced283e00f24dc77e8f2bf9c81c55f892c12ef90
Filipe FR <85907079+FilipeRaitz@users.noreply.github.com>
2023-07-20 15:34:19 -0400

    alterations

---
+++ coder.js
@@ -0,0 +1,12 @@
+
+const lines = document.querySelectorAll(".line");
+
+function (lines) {
+    for (line of lines) {
+        line.
+        for (element of line) {
+            element.classList.add()
+        }
+
+    }
+} 
